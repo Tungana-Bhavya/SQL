838a7623d5dd33eb4510bf54e604d4913a5ebd34
TUNGANA BHAVYA <67670424+Tungana-Bhavya@users.noreply.github.com>
2024-12-01 21:24:32 +0530

    Add files via upload

---
+++ LEETCODE/LEETCODE-SQL50/L_GROUP_SOLD_PRODUCTS_BY_DATE.sql
@@ -0,0 +1,8 @@
+GROUP SOLD PRODUCTS BY DATE
+------
+
+SELECT sell_date, COUNT(DISTINCT product) num_sold,
+GROUP_CONCAT(DISTINCT product ORDER BY product SEPARATOR ',') products
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date;
